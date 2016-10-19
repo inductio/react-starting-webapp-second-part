edd53bbc35338ac1bc283b095865eec0d55766c7
Aleksandr Shustrov <aleksandr.shustrov@gmail.com>
2016-10-19 19:42:42 +0300

    dev tool and debug

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,32 @@
-/**
- * Created by Oleksandr_Shustrov on 4/4/2016.
- */
-'use strict';
+var path = require('path');
+var webpack = require('webpack');
+
 module.exports = {
     devtool: 'eval-source-map',
-    module: {
-        loaders: [
-            {
-                test: /\.jsx?$/,
-                loader: 'babel',
-                exclude: /node_modules/,
-                query: {
-                    presets: ['react', 'es2015']
-                }
-            }
-        ]
-    },
     entry: {
         main: [
+            'react-hot-loader/patch',
+            'webpack-dev-server/client?http://localhost:8080',
+            'webpack/hot/only-dev-server',
             './src/main.js'
         ]
     },
     output: {
-        filename: './public/[name].js'
+        filename: './public/[name].js',
+        path: path.join(__dirname, 'public'),
+        publicPath: '/public/'
     },
-}
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin()
+    ],
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                include: path.join(__dirname, 'src'),
+                loader: 'babel'
+            }
+        ]
+    }
+};
